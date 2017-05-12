819ccda11b3b01eff21d4b099674f8be0388dd2f
Nigelk <kozlown@gmail.com>
2017-05-13 01:54:24 +0200

    handle disconnection, try to reconnect every 5s

--- mainServerInteractions/MainServerInteractions.js
+++ mainServerInteractions/MainServerInteractions.js
@@ -4,25 +4,27 @@ import File from 'File'
 import config from '../config'
 
 /**
- * @class Interactions
- * @description Interactions between server and main server
+ * @class MainServerInteractions
+ * @description Interactions between game and main server
  */
-class Interactions {
+class MainServerInteractions {
     /**
      * @constructor
      * @description constructor of a Interactions instance
      */
     constructor() {
-        this.ws = new WebSocket(`ws://${config.mainServer.host}:${config.mainServer.port}`)
-
-        this.ws.on('open', this.sayHello.bind(this))
-        this.ws.on('message', this.handleMessage.bind(this))
+        // try to connect to main server
+        this.connect(true)
     }
     /**
      * @method sayHello
      * @description say hello to the main server
      */
     sayHello() {
+        console.server('Successfully connected to main server !')
+        // clear reconnection interval
+        clearInterval(this.tryToReconnect)
+        // send information about the game server to main server
         const fileReader = new FileReader()
         fileReader.on('data', (data) => {
             const icon = data
@@ -50,20 +52,26 @@ class Interactions {
     handleMessage(data) {
         const dataObject = JSON.parse(data)
         switch (dataObject.route) {
-            case 'hello':
-                this.handleHello(dataObject)
-                break
             default:
                 break
         }
     }
     /**
-     * @method handleHello
-     * @description handle hello route response from main server
-     * @param {Object} data data received from the server
+     * @method connect
+     * @description try to connect and handle disconnection
      */
-    handleHello(data) {
-        console.info(data)
+    connect(firstConnection) {
+        setTimeout(() => {
+            console.server('trying to connect to main server...')
+            this.ws = new WebSocket(`ws://${config.mainServer.host}:${config.mainServer.port}`)
+            this.ws.on('error', (e) => {
+                console.error(e.message)
+            })
+            this.ws.on('open', this.sayHello.bind(this))
+            this.ws.on('message', this.handleMessage.bind(this))
+            // try to reconnect on close
+            this.ws.on('close', this.connect.bind(this))
+        }, firstConnection === true ? 0 : 5000)
     }
 }
-export default Interactions
+export default MainServerInteractions
